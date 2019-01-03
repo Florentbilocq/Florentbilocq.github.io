9a25e37a27a0db3285f48414ac6b997246b93fb1
Florent <florentbilocq@gmail.com>
2019-01-03 22:12:03 +0100

    pop up +redirection

---
+++ scripts/main.js
@@ -0,0 +1,12 @@
+function a() 
+{ if ( confirm( "veux tu aller sur le site xxxvidsxxx? Il est trop bien. Viens dessus stp please" ) ) {
+   document.location.href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+} else {
+    // Code à éxécuter si l'utilisateur clique sur "Annuler" 
+}}
+
+
+setTimeout(a,10000);
+
+
+
